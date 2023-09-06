2195d148beb5d1084390eb5b3699c6d5625da8c5
Taehoon <mysoul98@gmail.com>
2023-09-06 18:53:07 +0900

    feat: 로그인 기능 개발

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const PORT = 8000;
 app.set('view engine', 'ejs');
 app.set('/views', 'views');
 
+app.get('/', (req, res) => {
+  res.render('login');
+});
+
 app.listen(PORT, () => {
   console.log('Database connection succeeded!');
   console.log(`http://localhost:${PORT}`);
