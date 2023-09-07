b53adc1546ffc34db59fed179d376df0474de0ef
Taehoon <mysoul98@gmail.com>
2023-09-07 15:33:09 +0900

    feat: 로그인 모달창 기능 구현 및 css 적용

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const PORT = 8000;
 
 app.set('view engine', 'ejs');
 app.set('/views', 'views');
+app.use('/static', express.static(__dirname + '/static'));
 
 app.get('/', (req, res) => {
   res.render('login');
