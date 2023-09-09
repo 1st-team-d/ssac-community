ccd17b614ee008390d4d027cb62819ba21201cd1
taehoon <t11kim@yonsei.ac.kr>
2023-09-09 23:56:04 +0900

    feat: 로그인, 회원가입 유효성 검사, css 수정, 화면 이동 시 기능 추가

--- static/js/loginRegister.js
+++ static/js/loginRegister.js
@@ -3,22 +3,16 @@
 
 // 모달창 내용 초기화: 다른 모달창으로 전환 시 기존 input에 입력했던 데이터 초기화
 $('.modal').on('hidden.bs.modal', function () {
+  // hidden.bs.modal: 모달의 닫힘이 끝나고 실행되는 이벤트
+  // 폼, 메세지 초기화
   $(this).find('form')[0].reset();
+  $('.errorMsg').html('');
+  $('.checkEmailMsg').html('');
 });
 
 // 메세지 나타내는 요소(div)
 const errorMsg = document.querySelectorAll('.errorMsg');
 const checkEmailMsg = document.querySelector('#checkEmailMsg');
-
-// 사용자가 input 값 변경할 때 change 이벤트 발생
-// const loginEmail = document.querySelector('#loginEmail');
-// const loginPw = document.querySelector('#loginPw');
-// loginEmail.addEventListener('change', () => {
-//   errorMsg.remove();
-// });
-// loginPw.addEventListener('change', () => {
-//   errorMsg.remove();
-// });
 
 // 로그인 모달 창에서 로그인 버튼 누르면 폼 전송 -> 성공하면 main.ejs 리다이렉트
 async function postLogin() {
@@ -29,19 +23,25 @@ async function postLogin() {
   };
   // 프론트 유효성 검사
   if (!loginData.email || !loginData.pw) {
+    // 이메일, 비밀번호 빈칸으로 로그인 시도시 에러 메세지
     errorMsg[0].innerHTML = '이메일, 비밀번호는 필수 값입니다!';
   } else {
+    // *back*
+    // 입력 데이터로 로그인 시도 -> response로 true, false 반환 부탁드립니다.
     // const res = await axios({
-    //   url: '/login',
+    //   url: '/user/signin',
     //   method: 'post',
     //   data: loginData,
     // });
-    res = { data: true };
+    // 임시로 설정해놓은 값입니다.
+    res = { data: false };
     if (res.data) {
       // true -> 로그인 성공 -> 메인 화면으로 이동
       document.location.href = '/';
     } else {
-      errorMsg.innerHTML = '로그인 실패! 올바른 정보를 입력해주세요 :)';
+      // false -> 로그인 실패 -> 에러 메세지
+      errorMsg[0].innerHTML =
+        '이메일 혹은 비밀번호가 일치하지 않습니다. 다시 입력해 주세요.';
       // 로그인 모달창에서 오류 메세지 표시
     }
   }
@@ -49,6 +49,9 @@ async function postLogin() {
 
 // 회원가입 모달 창에서 회원가입 버튼 누르면 폼 전송 -> 성공하면 main.ejs 리다이렉트
 async function postRegister() {
+  // 회원가입 버튼 누르면 기존 에러 메세지 초기화
+  $('.checkEmailMsg').html('');
+  $('.errorMsg').html('');
   const form = document.forms['register'];
   const registerData = {
     name: form.registerName.value,
@@ -56,17 +59,44 @@ async function postRegister() {
     pw: form.registerPw.value,
   };
   if (!registerData.email || !registerData.pw || !registerData.name) {
+    // 빈 값인지 확인
     errorMsg[1].innerHTML = '이름, 이메일, 비밀번호는 필수 값입니다!';
   } else {
-    // const res = await axios({
-    //   url: '/register',
-    //   method: 'post',
-    //   data: registerData,
-    // });
-    res = { data: false };
-    if (res.data) {
-      // true -> 회원가입 성공 -> 메인 화면으로 이동
-      document.location.href = '/';
+    // 이메일 유효성 검사
+    const emailTest = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailTest.test(registerData.email)) {
+      errorMsg[1].innerHTML = '이메일 형식이 올바르지 않습니다.';
+    } else {
+      // 비밀번호 유효성 검사
+      const pwTest =
+        /^(?=.*[A-Za-z])(?=.*[!@#\$%\^&\*\(\)_\+\-=,./:;<>\?{}\[\]|~\\])(?=.*[0-9])\S{8,}$/;
+      if (!pwTest.test(registerData.pw)) {
+        errorMsg[1].innerHTML =
+          '비밀번호는 대소문자, 특수문자, 숫자를 포함한 8자리 이상';
+      } else {
+        // 중복체크 하지 않았을 경우
+        if (
+          !(
+            document.querySelector('.checkEmailMsg').textContent ===
+            '사용 가능한 이메일입니다.'
+          )
+        ) {
+          errorMsg[1].innerHTML = '이메일 중복체크를 해주세요.';
+        } else {
+          // *back*
+          // 회원가입 성공시 true, 실패시 false 응답 해주세요. 아마 다 true 뜰듯?
+          // const res = await axios({
+          //   url: '/user/signup',
+          //   method: 'post',
+          //   data: registerData,
+          // });
+          res = { data: true };
+          if (res.data) {
+            // true -> 회원가입 성공 -> 메인 화면으로 이동
+            document.location.href = '/';
+          }
+        }
+      }
     }
   }
 }
@@ -75,16 +105,20 @@ async function postRegister() {
 async function checkDuplicate(btn) {
   const form = document.forms['register'];
   const email = form.registerEmail.value;
+  // *back*
+  // 입력한 이메일로 중복 체크 -> 중복 아니면 true, 중복이면 false 응답 부탁드려요
   // const res = await axios({
-  //   url: '/checkemail',
+  //   url: '/checkEmail',
   //   method: 'post',
   //   data: email,
   // });
   res = { data: true };
   if (res.data) {
-    checkEmailMsg.innerHTML = '사용 가능한 이메일입니다!';
-    btn.innerHTML = '중복체크 완료';
+    checkEmailMsg.innerHTML = '사용 가능한 이메일입니다.';
+    checkEmailMsg.style.color = 'blue';
   } else {
-    checkEmailMsg.innerHTML = '다른 이메일을 입력 해주세요!';
+    checkEmailMsg.innerHTML =
+      '이미 사용중인 이메일입니다. 다른 이메일을 입력 해주세요.';
+    checkEmailMsg.classList.add('errorMsg');
   }
 }
