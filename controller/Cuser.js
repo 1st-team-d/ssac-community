56b0790af0cf3dfdaba3eccdccceb905165709d8
JeongYun Kim <95032287+pipi-shortstocking@users.noreply.github.com>
2023-09-10 03:34:52 +0900

    feat: 이메일(아이디), 비밀번호 유효성 검사 적용 완료

--- controller/Cuser.js
+++ controller/Cuser.js
@@ -1,16 +1,25 @@
 // User 모델 모듈 불러오기
-const e = require("express");
+const express = require("express");
 const { User } = require("../models");
 // bcrypt 패키지 불러오기
 const { hashedPw, comparePw } = require("../utils/encrypt");
 
 // 이메일(아이디) 유효성 검사 함수
+const checkEmail = async (email) => {
+  const regexr = /[\w\-\.]+\@[\w\-\.]+/g; // aaa@bbb.com
+
+  if (email.match(regexr)) {
+    return true; // 형식 통과
+  } else {
+    return false; // 형식 실패
+  }
+};
 
 // 비밀번호 유효성 검사 함수
-const checkGap = async (value) => {
-  const pwPattern = /\s/g; // 공백, 탭
+const checkPw = async (pw) => {
+  const regexr = /\s/g; // 공백, 탭
 
-  if (value.match(pwPattern)) {
+  if (pw.match(regexr)) {
     return false; // gap 존재
   } else {
     return true; // gap 존재 X
@@ -19,17 +28,17 @@ const checkGap = async (value) => {
 
 // 회원가입 처리 - 진행 중
 exports.postSignup = async (req, res) => {
-  // Todo: 유효성 검사(이메일(아이디) 중복, 비밀번호 공백 확인)
-
   try {
     // 회원가입 요청 시, 암호화된 비밀번호 DB 추가
     const { registerName, registerEmail, registerPw } = req.body;
 
+    // 이메일(아이디) 유효성 검사(형식 확인)
+    isCorrect = await checkEmail(registerEmail);
     // 비밀번호 유효성 검사(공백 확인)
-    isNoGap = await checkGap(registerPw);
+    isNoGap = await checkPw(registerPw);
 
-    if (isNoGap) {
-      // 비밀번호 유효성 검사 통과
+    if (isCorrect && isNoGap) {
+      // 이메일(아이디), 비밀번호 유효성 검사 통과
       const hashedPW = await hashedPw(registerPw); // 비밀번호 암호화
       await User.create({
         id: registerEmail,
@@ -37,10 +46,10 @@ exports.postSignup = async (req, res) => {
         name: registerName,
       }); // DB 추가
 
-      res.send({ isSignup: true });
+      res.send({ isCorrect, isSignup: true });
     } else {
-      // 비밀번호 유효성 통과 실패
-      res.send({ isNoGap });
+      // 이메일(아이디), 비밀번호 유효성 검사 통과 실패
+      res.send({ isCorrect, isNoGap });
     }
   } catch (err) {
     console.error(err);
@@ -54,17 +63,30 @@ exports.postCheckEmail = async (req, res) => {
     // 이메일(아이디)를 찾아서 회원 존재 유무 확인
     const { registerEmail } = req.body;
 
+    // 이메일(아이디) 유효성 검사(형식 확인)
+    isCorrect = await checkEmail(registerEmail);
+
     // DB 접근
     const user = await User.findOne({
       where: { id: registerEmail },
     });
 
-    if (user) {
-      // 이메일(아이디)가 중복되는 경우
-      res.send({ isCheck: false, msg: "이메일이 중복입니다!" });
+    if (isCorrect) {
+      // 이메일(아이디) 유효성 검사 통과
+      if (user) {
+        // 이메일(아이디)가 중복되는 경우
+        res.send({ isCorrect, isCheck: false, msg: "이메일이 중복입니다!" });
+      } else {
+        // 이메일(아이디)가 중복되지 않는 경우
+        res.send({
+          isCorrect,
+          isCheck: true,
+          msg: "이메일이 중복되지 않습니다!",
+        });
+      }
     } else {
-      // 이메일(아이디)가 중복되지 않는 경우
-      res.send({ isCheck: true, msg: "이메일이 중복되지 않습니다!" });
+      // 이메일(아이디) 유효성 검사 통과 실패
+      res.send({ isCorrect });
     }
   } catch (err) {
     console.error(err);
@@ -80,10 +102,12 @@ exports.postSignin = async (req, res) => {
     // 1. 이메일(아이디)를 찾아서 회원 존재 유무 확인
     const { loginEmail, loginPw } = req.body;
 
+    // 이메일(아이디) 유효성 검사(형식 확인)
+    isCorrect = await checkEmail(loginEmail);
     // 비밀번호 유효성 검사(공백 확인)
-    isNoGap = await checkGap(loginPw);
+    isNoGap = await checkPw(loginPw);
 
-    if (isNoGap) {
+    if (isCorrect && isNoGap) {
       // 비밀번호 유효성 통과
       // DB 접근
       const user = await User.findOne({
@@ -99,18 +123,28 @@ exports.postSignin = async (req, res) => {
         if (compareResult) {
           // 비밀번호 일치
           req.session.userInfo = { name: user.name, id: user.id }; // 회원 정보 세션 생성
-          res.send({ isNoGap, isSignin: true, data: user });
+          res.send({ isCorrect, isNoGap, isSignin: true, data: user });
         } else {
           // 비밀번호 불일치
-          res.send({ isNoGap, isSignin: false, msg: "비밀번호가 틀림" });
+          res.send({
+            isCorrect,
+            isNoGap,
+            isSignin: false,
+            msg: "비밀번호가 틀림",
+          });
         }
       } else {
         // 회원 없음
-        res.send({ isNoGap, isSignin: false, msg: "사용자가 존재하지 않음" });
+        res.send({
+          isCorrect,
+          isNoGap,
+          isSignin: false,
+          msg: "사용자가 존재하지 않음",
+        });
       }
     } else {
-      // 비밀번호 유효성 통과 실패
-      res.send({ isNoGap });
+      // 이메일(아이디), 비밀번호 유효성 검사 통과 실패
+      res.send({ isCorrect, isNoGap });
     }
   } catch (err) {
     console.error(err);
