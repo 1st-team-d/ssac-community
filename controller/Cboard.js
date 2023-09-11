a96f2a7f24cdbfb127472ea3d7010711d750c9be
eoeung <eoeung113@gmail.com>
2023-09-11 20:15:02 +0900

    Feat :: 게시글 등록, 수정 최종 완료

--- controller/Cboard.js
+++ controller/Cboard.js
@@ -9,28 +9,90 @@ exports.board = (req, res) => {
 // GET '/board/register'
 // 게시글 등록 화면으로 이동 // 수정 화면도 동일
 exports.getRegister = (req, res) => {
-  res.send('success');
+  res.render('board/postBoard');
 };
 
 // POST '/board/register'
 // 게시글 등록
-exports.postRegister = (req, res) => {
+exports.postRegister = async (req, res) => {
   try {
-    console.log('req.file ::::: ', req.file);
-    console.log('req.files ::::: ', req.files);
-    console.log('req.body ::::: ', req.body);
+    // ############### 파일 업로드 문제 없는지 확인 ###############
+    // console.log('req.file ::::: ', req.file); // single
+    const { fieldname, destination, filename } = req.file;
+    const imagePath = destination + filename;
+    // console.log('req.files ::::: ', req.files); // fields, array
+    // console.log('req.body ::::: ', req.body);
+
+    const jsonData = JSON.parse(req.body['data']); // 넘어온 JSON 데이터를 JS Object로 변환
+
+    // console.log('jsonData ::::: ', jsonData);
+    // req.file.preFilepath = '/uploadImage/'; // userUpload 설정
+    const { title, content } = jsonData;
+
+    // console.log('title ::::: ', title);
+    // console.log('content ::::: ', content);
+    // console.log('imagePath ::::: ', imagePath);
+
+    // ############### DB 작업 ###############
+    const insertOneBoard = await Board.create({
+      title: title,
+      content: content,
+      imagePath: imagePath,
+    });
+
+    // console.log(insertOneBoard);
+
+    // res.send({ file: req.file, data: req.body });
+    res.send(insertOneBoard);
+    // res.send('hello')
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// GET '/board/modify'
+// 게시글 수정 화면
+exports.getModify = async (req, res) => {
+  try {
+    const { boardSeq } = req.body;
+    const selectOneBoard = await Board.findOne({
+      where: {
+        boardSeq: boardSeq,
+      },
+    });
+
+    res.render('board/postBoard', { result: selectOneBoard });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// PATCH '/board/modify'
+// 게시글 수정 처리
+exports.patchModify = async (req, res) => {
+  try {
+    // 이미지 업로드
+    const { fieldname, destination, filename } = req.file;
+    const imagePath = destination + filename;
 
     const jsonData = JSON.parse(req.body['data']);
+    const { title, content, boardSeq } = jsonData;
 
-    console.log('jsonData ::::: ', jsonData);
-    // req.file.preFilepath = '/uploadImage/'; // userUpload 설정
-    const { title, content, imagePath } = jsonData;
+    // DB 작업
+    const updateOneBoard = await Board.update(
+      {
+        title: title,
+        content: content,
+        imagePath: imagePath,
+      },
+      {
+        where: {
+          boardSeq: boardSeq,
+        },
+      }
+    );
 
-    console.log('title ::::: ', title);
-    console.log('content ::::: ', content);
-    console.log('imagePath ::::: ', imagePath);
-    // res.send({ file: req.file, data: req.body });
-    res.send('hello')
+    res.send(updateOneBoard);
   } catch (err) {
     console.log(err);
   }
--- routes/board.js
+++ routes/board.js
@@ -7,6 +7,11 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 
+// 파일 업로드 잘되는지 확인
+// const upload = multer({
+//   dest: 'uploads/',
+// });
+
 // 업로드 이미지 설정
 const uploadConfig = multer({
   storage: multer.diskStorage({
@@ -20,10 +25,11 @@ const uploadConfig = multer({
       if (month < 10) month = '0' + month;
       if (day < 10) day = '0' + day;
 
-      const isExist = fs.existsSync(`uploads/${year}${month}${day}`);
-      const folderName = `uploads/${year}${month}${day}/`;
-      
+      const isExist = fs.existsSync(`uploads/${year}${month}${day}`); // uploads/YYYYMMDD 폴더가 있는지 확인
+      const folderName = path.join(`uploads/${year}${month}${day}`, '/'); // 폴더명은 'uploads/YYYYMMDD'의 형식
+
       if (!isExist) {
+        // 만약 YYYYMMDD 폴더가 존재하지 않으면 폴더를 새로 생성
         fs.mkdirSync(folderName, { recursive: true });
       }
 
@@ -31,7 +37,7 @@ const uploadConfig = multer({
     },
     filename(req, file, callback) {
       const random = Math.trunc(Math.random() * Math.pow(10, 15)); // 임의의 15자리 숫자를 가지고 온다.
-      console.log(random);
+      //   console.log(random);
       const ext = path.extname(file.originalname); // 확장자 추출
       const fileName = path.basename(file.originalname, ext) + random + ext; // 파일명
       // Ex) apple.png → apple40195724.png
@@ -43,10 +49,6 @@ const uploadConfig = multer({
   },
 });
 
-const upload = multer({
-  dest: 'uploads/',
-});
-
 router.get('/', controller.board); // 게시글 목록 화면
 
 router.get('/register', controller.getRegister); // 게시글 등록 화면
@@ -54,6 +56,12 @@ router.post(
   '/register',
   uploadConfig.single('uploadImage'),
   controller.postRegister
-); // 게시글 등록
+); // 게시글 등록 처리
+router.get('/modify', controller.getModify); // 게시글 수정 화면
+router.patch(
+  '/modify',
+  uploadConfig.single('uploadImage'),
+  controller.patchModify
+); // 게시글 수정 처리
 
 module.exports = router;
