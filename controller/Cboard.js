928b400717365314c273f8b507a517dda246b845
pipi-shortstocking <jannie435@naver.com>
2023-09-11 17:33:07 +0900

    feat: 보드 조회/삭제 진행 중

--- controller/Cboard.js
+++ controller/Cboard.js
@@ -1,3 +1,19 @@
-exports.board = (req, res) => {
-  res.render('index'); //임시로 index 렌더 -> 나중에 바꿔야함
+const { Board } = require("../models");
+
+// 게시글 조회 화면 - 진행 중
+exports.getBoard = async (req, res) => {
+  // res.render("board/postBoard"); // 임시로 postBoard로 등록 -> 페이지 완성시 페이지 교체 필요
+};
+
+// 특정 게시글 화면 - 진행 중
+exports.getBoardId = async (req, res) => {
+  // 특정 게시글의 게시글 시퀀스
+  const { boardSeq } = req.params;
+
+  // DB 접근
+  const seq = Board.findOne({
+    where: { boardSeq: boardSeq },
+  });
+
+  // res.render("")
 };
--- routes/board.js
+++ routes/board.js
@@ -1,8 +1,17 @@
 // '/board'로 오는 모든 요청은 여기서 처리
-const express = require('express');
-const controller = require('../controller/Cboard');
+const express = require("express");
+const controller = require("../controller/Cboard");
 const router = express.Router();
 
-router.get('/', controller.board); // 게시글 목록 화면
+// 게시글 조회 화면으로 이동
+router.get("/", controller.getBoard);
+
+// 특정 게시글 화면으로 이동 및 조회
+router.get("/:id", controller.getBoardId);
+
+// 게시글 검색 -> 조회
+// 게시글 수정 처리
+// 게시글 삭제 처리
+router.delete("/remove", controller.deleteBoard);
 
 module.exports = router;
