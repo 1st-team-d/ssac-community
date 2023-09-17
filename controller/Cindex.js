2774b2d0f0620df56cd6fe116694f08367d04ef8
rlagywnd4 <rlagywnd4@gmail.com>
2023-09-17 23:29:09 +0900

    feat:메인 메뉴에 조회수 높은 게시글 5개 가져옴

--- controller/Cindex.js
+++ controller/Cindex.js
@@ -1,6 +1,19 @@
+const { Board } = require('../models');
+
 // GET '/'
 // 메인 화면
-exports.index = (req, res) => {
+exports.index = async (req, res) => {
   // console.log(req.session.userInfo);
-  res.render("index", { session: req.session.userInfo });
+  // 조회수가 높은 차례대로 글 5개 전달
+  const rankBoard = await Board.findAll({
+    order: [['count', 'DESC']],
+    limit: 5,
+  });
+  console.log('rank >>>>>', rankBoard);
+  // rankBoard 사용 예시 : 게시글의 조회수 출력
+  // for (i = 0; i < rankBoard.length; i++) {
+  //   console.log('rankBoard.count = ', rankBoard[i].count);
+  // }
+
+  res.render('index', { session: req.session.userInfo, boards: rankBoard });
 };
