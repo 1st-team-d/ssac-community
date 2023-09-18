83336ecf2e51c0bcfe53cdda45fa9e3cc40d78b4
pipi-shortstocking <jannie435@naver.com>
2023-09-18 18:59:03 +0900

    feat:관리자 유저 전체 조회

--- controller/Cadmin.js
+++ controller/Cadmin.js
@@ -1,9 +1,14 @@
+// User 모델 모듈 불러오기
+const { User } = require('../models');
+
 // GET '/admin'
 // 관리자 메인 화면
 exports.getAdmin = (req, res) => {
   res.render('admin/index');
 };
 
-exports.getUser = (req, res) => {
-  res.render('admin/user');
+exports.getUser = async (req, res) => {
+  const user = await User.findAll();
+
+  res.render('admin/user', { users: user });
 };
