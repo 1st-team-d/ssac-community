9d142ecc57c8488b2ce4a9e6bf096a4d17830c1e
taehoon <mysoul98@gmail.com>
2023-09-19 23:59:42 +0900

    feat: 스터디 관리 페이지 디자인

---
+++ static/js/listStudy.js
@@ -0,0 +1,20 @@
+// // 각 스터디 호버 했을 때 이동하기 버튼
+// const ithStudy = document.querySelectorAll('.ithStudy');
+// console.log(ithStudy);
+// ithStudy.forEach((study) => {
+//   const moveBtn = study.lastElementChild;
+//   study.addEventListener('mouseover', () => {
+//     setTimeout(() => {
+//       moveBtn.textContent = '이동하기';
+//       moveBtn.style.backgroundColor = 'green';
+//       moveBtn.style.display = 'block';
+//       moveBtn.style.transition = 'transform 1s ease';
+//     }, 500);
+//   });
+//   study.addEventListener('mouseleave', () => {
+//     setTimeout(() => {
+//       moveBtn.style.display = 'none';
+//       moveBtn.style.transition = 'transform 1s ease';
+//     }, 500);
+//   });
+// });
