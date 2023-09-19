0f380e411986589d2e716de54578484652f09dd2
JeongYun Kim <95032287+pipi-shortstocking@users.noreply.github.com>
2023-09-19 15:15:32 +0900

    feat: 모든 유저 조회 완료, 특정 유저 조회 진행 중

--- controller/Cadmin.js
+++ controller/Cadmin.js
@@ -1,5 +1,5 @@
 // User 모델 모듈 불러오기
-const { User } = require('../models');
+const { User, sequelize } = require('../models');
 
 // GET '/admin'
 // 관리자 메인 화면
@@ -8,7 +8,19 @@ exports.getAdmin = (req, res) => {
 };
 
 exports.getUser = async (req, res) => {
-  const user = await User.findAll();
+  const user = await User.findAll({
+    attributes: [
+      'userSeq',
+      'id',
+      'name',
+      'isAdmin',
+      [sequelize.fn('YEAR', sequelize.col('user.createdAt')), 'year'],
+      [sequelize.fn('MONTH', sequelize.col('user.createdAt')), 'month'],
+      [sequelize.fn('DAY', sequelize.col('user.createdAt')), 'day'],
+      'createdAt',
+      'updatedAt',
+    ],
+  });
 
   res.render('admin/user', { users: user });
 };
