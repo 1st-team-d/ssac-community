ef02832961319d218b703461f3ce6634f4410fa8
Taehoon <mysoul98@gmail.com>
2023-09-21 18:59:50 +0900

    feat: 게시물 목록 페이징 처리

--- static/js/listBoard.js
+++ static/js/listBoard.js
@@ -1,20 +1,8 @@
-// 페이지 번호 변경 및 화면 표시 게시물 변경 함수
-async function changePageNum(pageDiv) {
-  // 해당 페이지 번호 클릭 -> 클릭한 this 객체가 pageDiv
-  const pageNum = pageDiv.textContent;
+// 요소 추가하는 함수 반복돼서 분리
+function updateElement(boards) {
   const boardList = document.getElementById('boardList');
 
   boardList.innerHTML = ''; // 기존 목록을 비우고 검색 결과를 새로 표시
-
-  // 해당 페이지에 해당하는 데이터 보내달라고 요청
-  const res = await axios({
-    url: `/board/list?pageNum=${pageNum}`,
-    method: 'get',
-  });
-  console.log(res.data);
-  // 서버에서 전송한 게시물 데이터
-  let boards = res.data.data;
-
 
   if (boards) {
     boards.forEach((board) => {
@@ -30,7 +18,7 @@ async function changePageNum(pageDiv) {
 
       // innerHTML로 아예 갈아 엎어서 페이지 누를때마다 새로 집어넣기
       const html = `
-      <a href="/board?boardSeq=${boardSeq}" class="view-link">
+      <a href="/board/list?boardSeq=${boardSeq}" class="view-link">
         <div class="card mb-3">
           <div class="card-body row">
             <div class="board_info text-start fw-bold col-6">
@@ -51,54 +39,24 @@ async function changePageNum(pageDiv) {
       div.innerHTML = html;
       // 생성된 요소를 목록에 추가
       boardList.append(div);
-    })
-  } else {
-    innerHTML = `<div class="col-12">게시글이 없습니다.</div>`;
-  }
-  newDiv.innerHTML = innerHTML;
-  newDivs.append(newDiv);
-  console.log(newDivs);
-  document.querySelector('#boardList').innerHTML = newDivs.innerHTML;
-  console.log(boardList);
-
-  // boardList 업데이트 하기 위한 newDivs
-  const newDivs = document.createElement('div'); // boardList에 바꿔줄 div 여러개 담고 있는
-
-  // 하나하나의 게시글 정보를 담을 div
-  const newDiv = document.createElement('div');
-  newDiv.classList.add('row', 'py-2');
-  let innerHTML = '';
-
-  // 페이지 변경했을 때, 값이 있는 경우
-  if (boards) {
-    boards.forEach((board, index) => {
-      const count = board.count;
-      const title = board.title;
-      const boardSeq = board.boardSeq;
-      const year = board.year;
-      const month = board.month;
-      const day = board.day;
-
-      // innerHTML로 아예 갈아 엎어서 페이지 누를때마다 새로 집어넣기
-      innerHTML = `
-        <div class="num col-2">${(pageNum - 1) * 5 + 1 + index}</div>
-        <div class="title col-6">
-            <a href="/board?boardSeq=${boardSeq}" class="view-link">${title}</a>
-        </div>
-        <div class="date col-2">${year}/${month}/${day}</div>
-        <div class="count col-2">${count}</div>
-      `;
     });
-    // 페이지 변경했을 때, 값이 없는 경우, '게시글이 없습니다.' 출력
   } else {
     innerHTML = `<div class="col-12">게시글이 없습니다.</div>`;
   }
-  newDiv.innerHTML = innerHTML;
-  newDivs.append(newDiv);
-  console.log(newDivs);
-  document.querySelector('#boardList').innerHTML = newDivs.innerHTML;
+}
 
-  // newDivs의 innerHTML 이 곧 num, title ~~ 이런거니까 이걸 계속 바꿔주면 됨.
+// 페이지 번호 변경 및 화면 표시 게시물 변경 함수
+async function changePageNum(pageDiv) {
+  // 해당 페이지 번호 클릭 -> 클릭한 this 객체가 pageDiv
+  const pageNum = pageDiv.textContent;
+
+  // 해당 페이지에 해당하는 데이터 보내달라고 요청
+  const res = await axios({
+    url: `/board/list?pageNum=${pageNum}`,
+    method: 'get',
+  });
+  let boards = res.data.data;
+  updateElement(boards);
 }
 
 // 검색 버튼 요소 가져오기
@@ -112,7 +70,7 @@ const performSearch = async () => {
   const sanitizedKeyword = keyword.trim(); // ""
 
   if (!sanitizedKeyword) {
-    // !"" -> !false -> true
+    document.location.reload();
     return;
   }
 
@@ -132,7 +90,7 @@ const performSearch = async () => {
         let allSearchLen = data.data.length;
         console.log(allSearchLen, data.data);
         const newDivs = document.createElement('div');
-        for (i = 0; i < Math.ceil(allSearchLen / 5); i++) {
+        for (i = 0; i < Math.ceil(allSearchLen / 10); i++) {
           const newDiv = document.createElement('div');
           newDiv.setAttribute('onclick', 'changePageNum(this)');
           newDiv.textContent = `${i + 1}`;
@@ -177,7 +135,7 @@ function renderSearchResults(results) {
       // 새로운 게시물 요소 생성
       const div = document.createElement('div');
       const html = `
-      <a href="/board?boardSeq=${boardSeq}" class="view-link">
+      <a href="/board/list?boardSeq=${boardSeq}" class="view-link">
         <div class="card mb-3">
           <div class="card-body row">
             <div class="board_info text-start fw-bold col-6">
@@ -199,7 +157,7 @@ function renderSearchResults(results) {
 
       // 생성된 요소를 목록에 추가
       boardList.append(div);
-    })
+    });
   } else {
     boardElement.innerHTML = `<div class="col-12">검색된 게시글이 없습니다.</div>`;
   }
@@ -214,7 +172,7 @@ let input = document.querySelector('input[name="category"]'),
   tagify = new Tagify(input, {
     enforceWhitelist: true,
     editTags: false,
-    whitelist: ['웹', '앱', 'AI', 'UI / UX', '게임', '기타', '전체'],
+    whitelist: ['웹', '앱', 'AI', 'UI / UX', '게임', '기타'],
     maxTags: 6,
     dropdown: {
       maxItems: 6, // <- mixumum allowed rendered suggestions
@@ -283,10 +241,70 @@ async function onAddTag(e) {
     method: 'get',
     params: { category: getCategories },
   });
+  if (res.data.board) {
+    const boards = res.data.board;
+    const boardList = document.getElementById('boardList');
+
+    boardList.innerHTML = ''; // 기존 목록을 비우고 검색 결과를 새로 표시
+
+    boards.forEach((board) => {
+      const count = board.board.count;
+      const title = board.board.title;
+      const content = board.board.content;
+      const boardSeq = board.board.boardSeq;
+      const year = board.board.year;
+      const month = board.board.month;
+      const day = board.board.day;
+      const boardLength = boards.length;
+
+      const div = document.createElement('div');
+
+      // innerHTML로 아예 갈아 엎어서 페이지 누를때마다 새로 집어넣기
+      const html = `
+      <a href="/board/list?boardSeq=${boardSeq}" class="view-link">
+        <div class="card mb-3">
+          <div class="card-body row">
+            <div class="board_info text-start fw-bold col-6">
+              ${title}
+              <span class="badge bg-secondary"
+                >${year + '.' + month + '.' + day}</span
+              >
+              <span class="badge bg-secondary">view ${count}</span>
+            </div>
+
+            <div class="content col-12 text-start text-wrap gy-3">
+                ${content <= 180 ? content : content.slice(0, 179) + ' ...'}
+            </div>
+          </div>
+        </div>
+      </a>
+      `;
+      div.innerHTML = html;
+      // 생성된 요소를 목록에 추가
+      boardList.append(div);
+      // 페이징 처리
+      // 페이징 처리
+      const boardPage = document.querySelector('.board_page');
+      boardPage.innerHTML = '';
+      const newDiv = document.createElement('div');
+      for (i = 0; i < Math.ceil(boardLength / 10); i++) {
+        const pageDiv = document.createElement('div');
+        pageDiv.setAttribute('onclick', 'changePageNum(this)');
+        pageDiv.textContent = i + 1;
+        newDiv.append(pageDiv);
+      }
+      console.log('뉴디브', newDiv);
+      boardPage.innerHTML = newDiv.innerHTML;
+    });
+  } else {
+    const boardList = document.getElementById('boardList');
+    const html = `<div class="col-12">게시글이 없습니다.</div>`;
+    boardList.innerHTML = html;
+  }
 }
 
 // tag removed callback
-function onRemoveTag(e) {
+async function onRemoveTag(e) {
   console.log('onRemoveTag:', e.detail, 'tagify instance value:', tagify.value);
   // 카테고리 삭제되었으므로 현재 input의 모든 카테고리 값으로 조회
   // 선택된 카테고리 파싱
@@ -317,11 +335,122 @@ function onRemoveTag(e) {
     }
   });
   console.log('카테고리>>>>', getCategories);
-  axios({
+  const res = await axios({
     url: '/board/list',
     method: 'get',
     params: { category: getCategories },
   });
+  console.log(res.data);
+  if (res.data.board) {
+    const boards = res.data.board;
+    const boardList = document.getElementById('boardList');
+
+    boardList.innerHTML = ''; // 기존 목록을 비우고 검색 결과를 새로 표시
+
+    boards.forEach((board) => {
+      const count = board.board.count;
+      const title = board.board.title;
+      const content = board.board.content;
+      const boardSeq = board.board.boardSeq;
+      const year = board.board.year;
+      const month = board.board.month;
+      const day = board.board.day;
+      const boardLength = boards.length;
+
+      const div = document.createElement('div');
+
+      // innerHTML로 아예 갈아 엎어서 페이지 누를때마다 새로 집어넣기
+      const html = `
+      <a href="/board/list?boardSeq=${boardSeq}" class="view-link">
+        <div class="card mb-3">
+          <div class="card-body row">
+            <div class="board_info text-start fw-bold col-6">
+              ${title}
+              <span class="badge bg-secondary"
+                >${year + '.' + month + '.' + day}</span
+              >
+              <span class="badge bg-secondary">view ${count}</span>
+            </div>
+
+            <div class="content col-12 text-start text-wrap gy-3">
+                ${content <= 180 ? content : content.slice(0, 179) + ' ...'}
+            </div>
+          </div>
+        </div>
+      </a>
+      `;
+      div.innerHTML = html;
+      // 생성된 요소를 목록에 추가
+      boardList.append(div);
+      // 페이징 처리
+      const boardPage = document.querySelector('.board_page');
+      boardPage.innerHTML = '';
+      const newDiv = document.createElement('div');
+      for (i = 0; i < Math.ceil(boardLength / 10); i++) {
+        const pageDiv = document.createElement('div');
+        pageDiv.setAttribute('onclick', 'changePageNum(this)');
+        pageDiv.textContent = i + 1;
+        newDiv.append(pageDiv);
+      }
+      console.log('뉴디브', newDiv);
+      boardPage.innerHTML = newDiv.innerHTML;
+    });
+  } else {
+    const boards = res.data.data;
+    const boardList = document.getElementById('boardList');
+
+    boardList.innerHTML = ''; // 기존 목록을 비우고 검색 결과를 새로 표시
+
+    const boardLength = res.data.allBoardLen;
+
+    boards.forEach((board) => {
+      const count = board.count;
+      const title = board.title;
+      const content = board.content;
+      const boardSeq = board.boardSeq;
+      const year = board.year;
+      const month = board.month;
+      const day = board.day;
+
+      const div = document.createElement('div');
+
+      // innerHTML로 아예 갈아 엎어서 페이지 누를때마다 새로 집어넣기
+      const html = `
+      <a href="/board/list?boardSeq=${boardSeq}" class="view-link">
+        <div class="card mb-3">
+          <div class="card-body row">
+            <div class="board_info text-start fw-bold col-6">
+              ${title}
+              <span class="badge bg-secondary"
+                >${year + '.' + month + '.' + day}</span
+              >
+              <span class="badge bg-secondary">view ${count}</span>
+            </div>
+
+            <div class="content col-12 text-start text-wrap gy-3">
+                ${content <= 180 ? content : content.slice(0, 179) + ' ...'}
+            </div>
+          </div>
+        </div>
+      </a>
+      `;
+      div.innerHTML = html;
+      // 생성된 요소를 목록에 추가
+      boardList.append(div);
+      // 페이징 처리
+      const boardPage = document.querySelector('.board_page');
+      boardPage.innerHTML = '';
+      const newDiv = document.createElement('div');
+      for (i = 0; i < Math.ceil(boardLength / 10); i++) {
+        const pageDiv = document.createElement('div');
+        pageDiv.setAttribute('onclick', 'changePageNum(this)');
+        pageDiv.textContent = i + 1;
+        newDiv.append(pageDiv);
+      }
+      console.log('뉴디브', newDiv);
+      boardPage.innerHTML = newDiv.innerHTML;
+    });
+  }
 }
 
 // invalid tag added callback
@@ -335,38 +464,4 @@ function onTagifyFocusBlur(e) {
 
 function onDropdownSelect(e) {
   console.log('onDropdownSelect: ', e.detail);
-
-  const boardElement = document.createElement('div');
-  boardElement.classList.add('row', 'py-2');
-
-  // 검색 결과 있는 경우
-  if (results.length > 0) {
-    // 검색 결과를 반복하여 목록에 추가
-    results.forEach((board, index) => {
-      const count = board.count;
-      const title = board.title;
-      // const createdAt = board.createdAt;
-      const boardSeq = board.boardSeq;
-      const year = board.year;
-      const month = board.month;
-      const day = board.day;
-
-      // 새로운 게시물 요소 생성
-      boardElement.innerHTML = `
-          <div class="num col-2">${index + 1}</div>
-          <div class="title col-6">
-              <a href="/board/list?boardSeq=${boardSeq}" class="view-link">${title}</a>
-          </div>
-          <div class="date col-2">${year}/${month}/${day}</div>
-          <div class="count col-2">${count}</div>
-      `;
-    });
-
-  // 검색한 값이 없는 경우, '검색된 게시글이 없습니다.' 출력
-  } else {
-    boardElement.innerHTML = `<div class="col-12">검색된 게시글이 없습니다.</div>`;
-  }
-
-  // 생성된 요소를 목록에 추가
-  boardList.appendChild(boardElement);
 }
